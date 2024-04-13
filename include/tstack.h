9e5664212beb28e36d750747f33322d4777da0b9
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-04-13 15:45:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,30 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+private:
+    T* array;
+    int beg;
+public:
+    tstack() : beg(-1) {
+        array = new T[size];
+    }
+    T get() const {
+        return array[beg];
+    }
+    bool empty() const {
+        return beg == -1;
+    }
+    bool isfull() const {
+        return beg == size - 1;
+    }
+    void pop() {
+        if (beg >= 0)
+            beg--;
+    }
+    void push(T a) {
+        if (beg < size - 1)
+            array[++beg] = a;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
