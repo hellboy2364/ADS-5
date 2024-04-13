a8611e5172dd9f2e135cd1f3fda9a9816ff548f2
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-04-13 16:03:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,87 @@
 
 std::string infx2pstfx(std::string inf) {
   // добавьте код
-  return std::string("");
+    std::string dig;
+    int counter = 0;
+    tstack <char, 100> post;
+    for (char i : inf) {
+        if (i >= '0' && i <= '9') {
+            counter++;
+            if (counter == 1) {
+                dig += i;
+                continue;
+            }
+            dig = dig + ' ' + i;
+        }
+        else if (i == '+' || i == '-' || i == '*' || i == '/' ||  i == '(' || i == ')') {
+            if (i == '(') {
+                post.push(i);
+            }
+            else if (post.empty()) {
+                post.push(i);
+            }
+            else if (pr(i) > pr(post.get())) {
+                post.push(i);
+            }
+            else if (i == ')') {
+                while (post.get() != '(') {
+                    dig = dig + ' ' + post.get();
+                    post.pop();
+                }
+                post.pop();
+            }
+            else {
+                int w = pr(i);
+                int z = pr(post.get());
+                while (!post.empty() && w <= z) {
+                    dig = dig + ' ' + post.get();
+                    post.pop();
+                }
+                post.push(i);
+            }
+        }
+    }
+    while (!post.empty()) {
+        dig = dig + ' ' + post.get();
+        post.pop();
+    }
+    return dig;
+    return std::string("");
 }
 
 int eval(std::string pref) {
   // добавьте код
-  return 0;
+    tstack <int, 100> num;
+    for (char k : pref) {
+        if (k >= '0' && k <= '9') {
+            num.push(k - '0');
+        }
+        else if (pr(k)) {
+            int z = num.get();
+            num.pop();
+            int w = num.get();
+            num.pop();
+            switch (k) {
+            case '+':
+                num.push(z + w);
+                break;
+            case '-':
+                num.push(w - z);
+                break;
+            case '*':
+                num.push(z * w);
+                break;
+            case '/':
+                num.push(w / z);
+                break;
+            default:
+                continue;
+            }
+        }
+        else {
+            continue;
+        }
+    }
+    return num.get();
+    return 0;
 }
